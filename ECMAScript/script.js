b2bd182d2d68250c94ab7c2b902b13f6ed3af625
ehabbasbous <ehabbasbous@gmail.com>
2025-10-15 10:24:10 +0200

    Add files via upload

---
+++ ECMAScript/script.js
@@ -0,0 +1,69 @@
+let user = {
+  name: "John",
+  years: 30,
+};
+let { name, years: age, isAdmin = false } = user;
+
+console.log(name);
+console.log(age);
+console.log(isAdmin);
+//ex2
+
+let planetName = "Earth";
+let currentVisitorName = "John";
+
+//ex3
+let phrase = "Hello";
+
+if (true) {
+  let user = "John";
+  function sayHi() {
+    alert(`${phrase}, ${user}`);
+  }
+}
+sayHi();
+
+//ex4
+let user = {};
+user.name = "John";
+user.surname = "Smith";
+user.name = "Pete";
+delete user.name;
+
+//ex5
+const user = {
+  name: "John",
+};
+
+user.name = "Pete";
+
+console.log(user.name);
+
+//ex6
+let salaries = {
+  Fred: 100,
+  Ted: 160,
+  Ghaith: 130,
+};
+
+let sum = 0;
+for (let key in salaries) {
+  sum += salaries[key];
+}
+
+console.log(sum);
+
+//ex7
+
+result = a + b < 4 ? "Below" : "Over";
+
+//ex8
+
+let message =
+  login == "Employee"
+    ? "Hello"
+    : login == "Director"
+    ? "Greetings"
+    : login == ""
+    ? "No login"
+    : "";
